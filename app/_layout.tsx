b6c145e040e3378044dda99d7517136810c0963c
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-01-17 21:45:09 +0200

    quotes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,18 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { useFonts } from 'expo-font'
-import 'react-native-reanimated';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import "react-native-reanimated";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    'EBGaramond': require('../assets/fonts/EB_Garamond/EBGaramond-VariableFont_wght.ttf'),
-    'EBGaramondBold': require('../assets/fonts/EB_Garamond/static/EBGaramond-Bold.ttf'),
-  })
+    EBGaramond: require("../assets/fonts/EB_Garamond/EBGaramond-VariableFont_wght.ttf"),
+    EBGaramondBold: require("../assets/fonts/EB_Garamond/static/EBGaramond-Bold.ttf"),
+  });
 
   if (!fontsLoaded) {
     return null;
@@ -24,7 +24,7 @@ export default function RootLayout() {
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(notifications)" options={{ headerShown: false }} />
-        <StatusBar style="auto"  />
+        <StatusBar style="auto" />
       </Stack>
     </SafeAreaView>
   );
